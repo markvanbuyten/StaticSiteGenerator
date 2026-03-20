ff4b95aec903fb4abb8bb27b65b5f8a06195bf8c
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-03-20 19:29:40 +0100

    4: added unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,17 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is a url", TextType.LINK, "https://www.boot.dev")
+        node4 = TextNode("This is a url", TextType.LINK, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertEqual(node3, node4)
+        self.assertNotEqual(node2, node4)
+
+if __name__ == "__main__":
+    unittest.main()
