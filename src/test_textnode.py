09dbdb449fc0065da5cb3732c29e7431e77d53a6
Mark Vanbuyten <markvanbuyten@gmail.com>
2026-03-24 20:57:07 +0100

    7: added a TextNode to HTMLNode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -12,6 +12,19 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node3)
         self.assertEqual(node3, node4)
         self.assertNotEqual(node2, node4)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+        node1 = TextNode("This is a url", TextType.LINK, "https://www.boot.dev")
+        html_node1 = text_node_to_html_node(node1)
+        self.assertEqual(html_node1.tag, "a")
+        self.assertEqual(html_node1.value, "This is a url")
+        
+        
 
 if __name__ == "__main__":
     unittest.main()
